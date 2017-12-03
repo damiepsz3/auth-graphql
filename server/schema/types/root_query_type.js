82d502e8b214818febafd2b1324bdcfbc0f19025
damiepsz3 <damiepsz3@gmail.com>
2017-12-02 22:59:57 -0500

    feat: :sparkles: User type and signup mut

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,8 +1,21 @@
 const graphql = require('graphql');
-const { GraphQLObjectType } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString
+} = graphql;
+const UserType = require('./user_type')
 
 const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType'
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { email: { type: GraphQLString }},
+      resolve(parentValue, args) {
+        return 'text'
+      }
+    }
+  }
 });
 
 module.exports = RootQueryType;
